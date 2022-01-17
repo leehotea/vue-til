b29a057901985dc6680be76f25fd14eadf564b3d
이호태 <lsky0607@nextweb.kr>
2022-01-17 09:37:46 +0900

    섹션 3. 라우터 & 컴포넌트 설계 #6

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div>
     <header>
       <router-link to="/login">로그인</router-link> |
-      <router-link to="/signup">가입</router-link>
+      <router-link to="/signup">회원가입</router-link>
     </header>
     <router-view></router-view>
   </div>
